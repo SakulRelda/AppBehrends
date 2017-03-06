67611f52ce43ff5eef4013fe1c1e263df0c2d7c7
TREND_DOM\Wickersheim <nico.wickersheim@trendswm.de>
2017-03-06 13:33:30 +0100

    Add new TODO File

---
+++ app/src/main/java/com/example/lukasadler/test1/TODO.java
@@ -0,0 +1,21 @@
+package com.example.lukasadler.test1;
+
+import android.widget.Toast;
+
+/**
+ * Created by Wickersheim on 03.03.2017.
+ */
+
+public class TODO {
+    //Things to do:
+
+    //TODO: Maschine lässt sich noch ohne Eingabe speichern - darf nicht möglich sein
+    //TODO: Maschine anlegen - ohne Eingabe zurück - Toast erscheint trotzdem
+    //TODO: Spinner (Dropdown) für Maschinentyp
+    //TODO: maxLength für Maschinenname @done
+    //TODO: Automatischer Login bei Anlegen eines neuen Users
+    //TODO: Login nicht auf jede Activiry geprüft
+    //TODO: Bei Tabwechsel ändert sich die Selektierung des Tabs nicht
+    //TODO: Maschinenübersicht - Zurücktaste beendet App - Frage nach Logout?
+
+}
